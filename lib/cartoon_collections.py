708ea55a19b82d0558d4f765dda88693f6730d8d
Beau Kim <beaukim3@gmail.com>
2023-03-14 09:49:27 -0400

    third deliverable

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -9,8 +9,11 @@ def summon_captain_planet(planeteer_calls):
     return [call.capitalize() + "!" for call in planeteer_calls]
 
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for call in list:
+        if len(call) > 4:
+            return True
+    return False
 
 
 def find_the_cheese():
