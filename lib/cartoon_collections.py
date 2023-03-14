08647cac3cd1d8372c84a805cb0cff64fa9244b8
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-03-14 09:40:18 -0400

    roll call dwarves

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    count = 1
+    for dwarf in dwarves:
+        print(f"{count}. {dwarf}")
+        count += 1
+
 
 def summon_captain_planet():
     pass
 
+
 def long_planeteer_calls():
     pass
+
 
 def find_the_cheese():
     pass
